05bbe33866a570694313c646e048b5ec87994979
DianaAdy <diana.ady@edu.312school.com>
2025-04-14 19:42:36 -0500

    changed workflow

---
+++ environments/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket  = "539247466139-bucket-dev"
+    key     = "dev.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+data "aws_eks_cluster_auth" "eks_cluster_auth" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+}
