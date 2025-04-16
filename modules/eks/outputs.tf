99611bca6e65f7dc62c178040053b1382192794b
DianaAdy <diana.ady@edu.312school.com>
2025-04-15 22:13:11 -0500

    jj

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -18,7 +18,7 @@ output "eks_cluster_version" {
   value       = aws_eks_cluster.eks_cluster.version
 }
 output "eks_cluster_certificate_authority_data" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
   description = "The certificate authority data of the EKS cluster"
 }
 
