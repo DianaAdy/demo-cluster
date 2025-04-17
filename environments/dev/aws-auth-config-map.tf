0adac18b63afb5bf4c5d184fbffbbe3a1b3d6e11
DianaAdy <diana.ady@edu.312school.com>
2025-04-17 16:55:41 -0500

    gg

--- environments/dev/aws-auth-config-map.tf
+++ environments/dev/aws-auth-config-map.tf
@@ -1,33 +1,36 @@
-
 data "aws_caller_identity" "current" {}
+
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
   }
 
-  depends_on = [
-    module.eks
-  ]
+  depends_on = [module.eks]
 
   data = {
-    mapRoles = <<EOT
-- rolearn: ${module.eks.eks_worker_role_arn}
-  username: system:node:{{EC2PrivateDNSName}}
-  groups:
-    - system:bootstrappers
-    - system:nodes
-    - system:masters
-- rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.github_actions_terraform_role}
-  username: terraform
-  groups:
-    - system:masters
-EOT
-    mapUsers = <<EOT
-- userarn: arn:aws:iam::160885273651:user/diana
-  username: diana
-  groups:
-    - system:masters
-EOT
+    mapRoles = yamlencode([
+      {
+        rolearn  = module.eks.eks_worker_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = [
+          "system:bootstrappers",
+          "system:nodes"
+        ]
+      },
+      {
+        rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.github_actions_terraform_role}"
+        username = "terraform"
+        groups   = ["system:masters"]
+      }
+    ])
+
+    mapUsers = yamlencode([
+      {
+        userarn  = "arn:aws:iam::160885273651:user/diana"
+        username = "diana"
+        groups   = ["system:masters"]
+      }
+    ])
   }
 }
